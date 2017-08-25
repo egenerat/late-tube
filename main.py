88e4b38e769d9fabb8be301617ed52c377e73880
egenerat <emile.generat@outlook.com>
2017-08-25 23:38:38 +0100

    Detect delays feature ongoing

--- main.py
+++ main.py
@@ -1,25 +1,88 @@
 import csv
 from datetime import datetime
 
+ESTIMATED_JOURNEY_DURATION = {
+    'Hammersmith (District, Piccadilly lines)': {
+        'Hatton Cross': 27,
+        'Barons Court': 1,
+        'West Kensington': 3
+    }
+}
+MINIMUM_DELAY_REFUND = 15
+
+
+# CSV Keys
+KEY_DATE = "Date"
+KEY_START_TIME = "Start Time"
+KEY_END_TIME = "End Time"
+KEY_JOURNEY = "Journey/Action"
+
+# TODO:
+# - handle the no touch-out case
 
 def parse_date(string_day, string_time):
-    return datetime.strptime('19-Aug-2017 19:33', '%d-%b-%Y %H:%M')
+    return datetime.strptime("{} {}".format(string_day, string_time), '%d-%b-%Y %H:%M')
+
+def is_delayed_journey(station_a, station_b, duration_dt):
+    is_delayed = False
+    effective_duration_minutes = duration_dt.seconds / 60
+    theoric_duration = None
+    if station_a in ESTIMATED_JOURNEY_DURATION:
+        tmp = ESTIMATED_JOURNEY_DURATION[station_a]
+        if station_b in tmp:
+            theoric_duration = tmp[station_b]
+    if not theoric_duration:
+        if station_b in ESTIMATED_JOURNEY_DURATION:
+            tmp = ESTIMATED_JOURNEY_DURATION[station_b]
+            if station_a in tmp:
+                theoric_duration = tmp[station_a]            
+    if not theoric_duration:
+        print("Could not find theoric duration from {} to {}".format(station_a, station_b))
+    else:
+        # print("Estimated duration: {}".format(theoric_duration))
+        delay = effective_duration_minutes - theoric_duration
+        is_delayed = delay >= MINIMUM_DELAY_REFUND 
+        if is_delayed:
+            print("DELAY! Estimated: {}, effective: {}. Delay {} minutes".format(theoric_duration, effective_duration_minutes, delay))
+    return is_delayed
+    
+
+def is_tube_journey(str_journey):
+    BUS_STRING_PATTERN = "Bus journey, route "
+    REFUND_PATTERN = "Oyster helpline refund"
+    return not (str_journey.startswith(BUS_STRING_PATTERN) or str_journey.startswith(REFUND_PATTERN))
 
 
-def calculate_duration():
-    return 0
+def extract_departure_arrival(journey_str):
+    JOURNEY_SEPARATOR = " to "
+    departure_station, arrival_station = journey_str.split(JOURNEY_SEPARATOR)
+    return departure_station, arrival_station
 
 def parse_csv(filename):
-    result = []
     with open(filename) as csvfile:
         next(csvfile)
         csvfile
         reader = csv.DictReader(csvfile)
         for row in reader:
-            result.append(row)
-            print(row['Date'], row['Start Time'])
+            if is_tube_journey(row[KEY_JOURNEY]):
+                start_date = row[KEY_DATE]
+                start_dt = parse_date(start_date, row[KEY_START_TIME])
+                end_dt = parse_date(start_date, row[KEY_END_TIME])
+                if end_dt < start_dt:
+                    # case arrival on the next day
+                    print("TODO handle the case")
+                duration = end_dt - start_dt
+                delayed_journeys += 1
+                departure_station, arrival_station = extract_departure_arrival(row[KEY_JOURNEY])
+                # print(row)
+                if is_delayed_journey(departure_station, arrival_station, duration):
+                    # print("Delayed!")
+                    pass
+                else:
+                    # print("OK")
+                    pass
+            
 
 if __name__ == '__main__':
     FILENAME = "test-data/input.csv"
     parse_csv(FILENAME)
-
