ccd84b0c7d9da41315a4370ba83a6a2202b43715
egenerat <emile.generat@outlook.com>
2017-08-25 23:38:38 +0100

    Detect delays feature ongoing

---
+++ test.py
@@ -0,0 +1,20 @@
+from main import calculate_duration, parse_date
+import datetime
+
+import unittest
+
+class TestLateTube(unittest.TestCase):
+
+    def test_calculate_duration(self):
+        self.assertEqual(0, 0)
+
+    def test_parse_date(self):
+    	dt = parse_date("19-Aug-2017", "19:33")
+    	self.assertEqual(datetime.datetime(2017, 8, 19, 19, 33), dt)
+
+    def test_parse_date_morning(self):
+    	dt = parse_date("19-Aug-2017", "09:33")
+    	self.assertEqual(datetime.datetime(2017, 8, 19, 9, 33), dt)
+
+if __name__ == '__main__':
+    unittest.main()
