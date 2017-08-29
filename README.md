dd78859a966ecf62f7d373fd34a4a89745a0bd8a
egenerat <emile.generat@outlook.com>
2017-08-29 23:27:40 +0100

    First version of file uploading

---
+++ README.md
@@ -0,0 +1,11 @@
+Create the virtualenv
+python -m venv env
+
+Activate the virtualenv
+env\Scripts\activate
+
+Install python dependencies
+pip install -r requirements.txt
+
+Start the server
+python main.py
--- main.py
+++ main.py
@@ -1,88 +1,47 @@
-import csv
-from datetime import datetime
+from flask import Flask, flash, request, redirect, url_for, send_from_directory
+import os
+from werkzeug import secure_filename
 
-ESTIMATED_JOURNEY_DURATION = {
-    'Hammersmith (District, Piccadilly lines)': {
-        'Hatton Cross': 27,
-        'Barons Court': 1,
-        'West Kensington': 3
-    }
-}
-MINIMUM_DELAY_REFUND = 15
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = set(['csv'])
 
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-# CSV Keys
-KEY_DATE = "Date"
-KEY_START_TIME = "Start Time"
-KEY_END_TIME = "End Time"
-KEY_JOURNEY = "Journey/Action"
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-# TODO:
-# - handle the no touch-out case
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file', filename=filename))
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <p><input type=file name=file>
+         <input type=submit value=Upload>
+    </form>
+    '''
 
-def parse_date(string_day, string_time):
-    return datetime.strptime("{} {}".format(string_day, string_time), '%d-%b-%Y %H:%M')
-
-def is_delayed_journey(station_a, station_b, duration_dt):
-    is_delayed = False
-    effective_duration_minutes = duration_dt.seconds / 60
-    theoric_duration = None
-    if station_a in ESTIMATED_JOURNEY_DURATION:
-        tmp = ESTIMATED_JOURNEY_DURATION[station_a]
-        if station_b in tmp:
-            theoric_duration = tmp[station_b]
-    if not theoric_duration:
-        if station_b in ESTIMATED_JOURNEY_DURATION:
-            tmp = ESTIMATED_JOURNEY_DURATION[station_b]
-            if station_a in tmp:
-                theoric_duration = tmp[station_a]            
-    if not theoric_duration:
-        print("Could not find theoric duration from {} to {}".format(station_a, station_b))
-    else:
-        # print("Estimated duration: {}".format(theoric_duration))
-        delay = effective_duration_minutes - theoric_duration
-        is_delayed = delay >= MINIMUM_DELAY_REFUND 
-        if is_delayed:
-            print("DELAY! Estimated: {}, effective: {}. Delay {} minutes".format(theoric_duration, effective_duration_minutes, delay))
-    return is_delayed
-    
-
-def is_tube_journey(str_journey):
-    BUS_STRING_PATTERN = "Bus journey, route "
-    REFUND_PATTERN = "Oyster helpline refund"
-    return not (str_journey.startswith(BUS_STRING_PATTERN) or str_journey.startswith(REFUND_PATTERN))
-
-
-def extract_departure_arrival(journey_str):
-    JOURNEY_SEPARATOR = " to "
-    departure_station, arrival_station = journey_str.split(JOURNEY_SEPARATOR)
-    return departure_station, arrival_station
-
-def parse_csv(filename):
-    with open(filename) as csvfile:
-        next(csvfile)
-        csvfile
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            if is_tube_journey(row[KEY_JOURNEY]):
-                start_date = row[KEY_DATE]
-                start_dt = parse_date(start_date, row[KEY_START_TIME])
-                end_dt = parse_date(start_date, row[KEY_END_TIME])
-                if end_dt < start_dt:
-                    # case arrival on the next day
-                    print("TODO handle the case")
-                duration = end_dt - start_dt
-                delayed_journeys += 1
-                departure_station, arrival_station = extract_departure_arrival(row[KEY_JOURNEY])
-                # print(row)
-                if is_delayed_journey(departure_station, arrival_station, duration):
-                    # print("Delayed!")
-                    pass
-                else:
-                    # print("OK")
-                    pass
-            
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
 
 if __name__ == '__main__':
-    FILENAME = "test-data/input.csv"
-    parse_csv(FILENAME)
+    app.run()
