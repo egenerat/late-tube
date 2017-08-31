1befab36551e237b7724c8ee86f735789baee868
egenerat <emile.generat@outlook.com>
2017-08-31 22:14:09 +0100

    Pages construction ongoing

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask, flash, request, redirect, url_for, send_from_directory
+from flask import Flask, flash, request, redirect, url_for, send_from_directory, render_template
+
 import os
 from werkzeug import secure_filename
 
@@ -7,11 +8,13 @@ ALLOWED_EXTENSIONS = set(['csv'])
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.debug = True
+app.secret_key = 'top_secret'
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
         # check if the post request has the file part
@@ -27,16 +30,24 @@ def upload_file():
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            # TODO: Analysis here
             return redirect(url_for('uploaded_file', filename=filename))
-    return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form method=post enctype=multipart/form-data>
-      <p><input type=file name=file>
-         <input type=submit value=Upload>
-    </form>
-    '''
+    return render_template('upload.html', late_journeys=[
+{
+    'departure': 'Hammersmith',
+    'destination': 'Hatton Cross',
+    'estimated_delay': 45,
+    'departure_date': '24th of July',
+    'departure_time': '14:56'
+},
+{
+    'departure': 'Picadilly Circus',
+    'destination': 'Westminster',
+    'estimated_delay': 36,
+    'departure_date': '17th of August',
+    'departure_time': '03:54'
+}
+])
 
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
